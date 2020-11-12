f7d823cc59e92913c0dd939e4c2ffa795daa2824
Sebastien FERNANDEZ <sebastien.fernandez@ens.uvsq.fr>
2020-11-12 09:52:43 +0100

    td3

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -17,7 +17,7 @@ def pluriel(temps):
     
 
 def afficheTemps(temps):
-    affiche = ("jour", "heure", "minute", "seconde")
+    affiche = ("année", "jour", "heure", "minute", "seconde")
     for i in range(0,len(temps)):
         print(temps[i], " ", affiche[i], pluriel(temps[i]), sep="", end=" ")
 
@@ -45,12 +45,111 @@ def demandeTemps():
     return temps
 
 
-temps = (3, 21, 10, 35)
-secondes = 335435
+def sommeTempslong(temps1, temps2):
+    j = temps1[0] + temps2[0]
+    h = temps1[1] + temps2[1]
+    m = temps1[2] + temps2[2]
+    s = temps1[3] + temps2[3]
+    somme = (j, h, m, s)
+    print(somme)
+
+    if int(somme[3]) > 59 :
+        s = somme [3] - 60
+        m += 1    
+    if int(somme[2]) > 59 :
+        m = somme[2] - 60
+        h += 1
+    if int(somme[1]) > 23 :
+        h = int(somme[1]) - 24
+        j += 1
+
+    somme = (j, h, m, s)
+    return(somme)
+
+
+def sommeTemps(temps1, temps2):
+    temps1Sec = tempsSecondes(temps1)
+    temps2Sec = tempsSecondes(temps2)
+    somme = temps1Sec + temps2Sec
+    somme = secondesTemps(somme)
+    return(somme)
+
+
+def proportionTemps(temps, proportion):
+    secondes = tempsSecondes(temps)
+    propSecondes = int(secondes * proportion)
+    propTemps = secondesTemps(propSecondes)
+    propTemps = afficheTemps(propTemps)
+    return(propTemps)
+
+
+def tempsDate(temps):
+    secondes = tempsSecondes(temps)
+    a = 2020 + (secondes // (3600*24*365))
+    j = (secondes % (3600*24*365)) // (3600*24)
+    h = ((secondes % (3600*24*365)) % (3600*24)) // 3600
+    m = (((secondes % (3600*24*365)) % (3600*24)) % 3600) // 60
+    s = (((secondes % (3600*24*365)) % (3600*24)) % 3600) % 60
+    date = (a, j, h, m, s)
+    return date
+
+
+def afficheDate(date):
+    afficheTemps(tempsDate(temps))
+
+
+def time():
+    import time
+
+    seconde = int(time.time())
+    print(seconde, "secondes")
+
+    temps = secondesTemps(seconde)
+    afficheTemps(temps)
+    print("affichetemps")
+
+    date = tempsDate(temps)
+    afficheDate(date)
+    print("affichedate")
+
+
+def bisextile(jour):
+    date = tempsDate(secondesTemps(jour * 3600 * 24))
+    anneeBisextile = 0
+    year = date[0]
+    print("année = ", year)
+    for i in range(2020, year+1):
+        if i % 4 == 0 and i %100 != 0 or i % 400 == 0 :
+            anneeBisextile += 1
+            print(i)
+        else :
+            pass
+    return anneeBisextile
+
+
+def nombreBisextile(jour):
+    date = tempsDate(secondesTemps(jour * 3600 * 24))
+    anneeBisextile = 0
+    year = date[0]
+    print("année = ", year)
+    for i in range(2020, year+1):
+        if i % 4 == 0 and i %100 != 0 or i % 400 == 0 :
+            anneeBisextile += 1
+        else :
+            pass
+    return anneeBisextile
+
+print(bisextile(20000))
 
 """
+temps = (2, 0, 36, 0)
+secondes = 335435
 print(tempsSecondes(temps), "secondes")
 temps = secondesTemps(secondes)
 afficheTemps(temps)
 afficheTemps(demandeTemps())
+print(sommeTemps((2, 3, 4, 25),(5, 22, 57, 1)))
+proportionTemps(temps, 0.2)
+temps = secondesTemps(1000000000)
+afficheDate(temps)
 """
